906a20c2591067fe10a990cece574d13e609b745
Anton Follinger <47656465+Hundmat@users.noreply.github.com>
2025-09-03 14:39:42 +0200

    50nodes

---
+++ 50nodes/50nodeTest.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e  # exit on error
+
+NETWORK_NAME=testnet
+NODE_COUNT=50
+IMAGE=busybox   # lightweight image for demo (has ping, nc, etc.)
+
+# Clean up if network exists
+if docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
+    docker network rm "$NETWORK_NAME"
+fi
+
+# Create network
+docker network create "$NETWORK_NAME"
+
+# Start containers
+for i in $(seq 1 "$NODE_COUNT"); do
+    docker run -dit --name "node$i" --network "$NETWORK_NAME" "$IMAGE" sh
+done
+
+echo "Started $NODE_COUNT containers in network '$NETWORK_NAME'."
+
+
+# In node 2
+## nc -l -p 1234 
+
+# In node 1
+## echo 'Hello from node1' | nc node2 1234
+
+
+
---
+++ 50nodes/reset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# Stop all running containers
+docker stop $(docker ps -aq)
+
+# Remove all containers
+docker rm $(docker ps -aq)
+
+
+docker rmi busybox
